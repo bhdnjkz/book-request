63352414fcff031b3fd3bab1d9220d5f57be0cf0
Богдан <norestwithwest@Overlord.local>
2023-04-10 17:48:19 +0300

    switched to kebab-case

--- routes/routes.js
+++ routes/routes.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const router = express.Router();
-const indexController = require('../controllers/indexController')
+const indexController = require('../controllers/index-controller')
 const bookController = require('../controllers/books-page-controller')
 
 router.get('/', indexController.get);
