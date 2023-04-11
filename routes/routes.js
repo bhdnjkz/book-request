951a11bf34b0e16442b0a31e3ccc8859b510c319
Богдан <norestwithwest@Overlord.local>
2023-04-11 12:26:54 +0300

    updated list-controller

--- routes/routes.js
+++ routes/routes.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const router = express.Router();
 const indexController = require('../controllers/index-controller')
-const bookController = require('../controllers/books-page-controller')
+const bookController = require('../controllers/books-list-controller')
 
 router.get('/', indexController.get);
 router.post('/', indexController.post);
